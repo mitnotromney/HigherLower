111e6641f1d70b097d85eb78c207f9e87995834e
mitnotromney <timharrisco@gmail.com>
2019-11-25 18:33:14 -0600

    Added methods for changing the behavior of the button. Expanded if statements to include reactions of there is no input and set the inputtype for the edittext to numbers

--- app/src/main/java/com/example/higherlower/MainActivity.java
+++ app/src/main/java/com/example/higherlower/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,27 +17,53 @@ public class MainActivity extends AppCompatActivity {
 
     int randomNumber;
     EditText mGuessInput;
+    TextView mCorrectView;
+    Button mButtonPress;
+    String message;
 
     public void generateRandomNumber() {
         Random rand = new Random();
         randomNumber = rand.nextInt(20) + 1;
     }
 
+    public void isPlaying() {
+        mCorrectView.setVisibility(View.INVISIBLE);
+        mButtonPress.setText("Guess");
+    }
+
+    public void hasWon() {
+        mCorrectView.setVisibility(View.VISIBLE);
+        mButtonPress.setText("Play Again!");
+    }
+
+    public void makeToast() {
+        Toast toast = new Toast(getApplicationContext());
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        toast.setGravity(Gravity.CENTER, 0,0);
+    }
+
     public void guess(View view) {
         Log.i("Info", Integer.toString(randomNumber));
-        Log.i("Entered Value", mGuessInput.getText().toString());
-        int guessValue = Integer.parseInt(mGuessInput.getText().toString());
-        String message;
-        if (guessValue > randomNumber) {
-            message = "Lower";
-        } else if (guessValue < randomNumber) {
-            message = "Higher!";
-        } else {
-            message = "You got it!";
-            generateRandomNumber();
-        }
 
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        if (!mGuessInput.getText().toString().equals("")) {
+
+            int guessValue = Integer.parseInt(mGuessInput.getText().toString());
+
+            if (guessValue > randomNumber) {
+                message = "Lower";
+                isPlaying();
+            } else if (guessValue < randomNumber) {
+                message = "Higher!";
+                isPlaying();
+            } else {
+                hasWon();
+                message = "You got it!";
+                generateRandomNumber();
+            }
+        } else {
+            message = "Please make a guess!";
+        }
+        makeToast();
     }
 
     @Override
@@ -45,7 +72,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mGuessInput = findViewById(R.id.guess_et);
+        mCorrectView = findViewById(R.id.correct_tv);
+        mButtonPress = findViewById(R.id.guess_bt);
         generateRandomNumber();
     }
-
 }
